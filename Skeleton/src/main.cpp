5b388126256100399376353bd790dfc3f4559588
Enkhtur Narantungalag <enarantu@umich.edu>
2018-07-13 13:43:38 -0400

    added skeleton for solutions

---
+++ Skeleton/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "solution.h"
+
+using namespace std;
+
+int main(){
+    hi();
+    return 0;
+}
---
+++ Skeleton/src/solution.cpp
@@ -0,0 +1,6 @@
+#include "solution.h"
+#include <iostream>
+using namespace std;
+void hi(){
+    cout << "Hello world" << endl;
+}
